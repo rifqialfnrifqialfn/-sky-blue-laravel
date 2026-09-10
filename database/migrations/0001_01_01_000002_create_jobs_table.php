8271ab4f0706c6b4980510552de8bed68bc7cb3b
dimasabdulrohman-xi-rpl2 <dimasabdulrohman84@gmail.com>
2026-09-11 00:06:21 +0700

    dimas abdul rohman

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -3,11 +3,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run igrations.
      */
     public function up(): void
     {
